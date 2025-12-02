40b977a4da95e2022fd7da65b621bb773900515c
Hunter Murphy <murphunt.developer@gmail.com>
2025-12-01 20:17:14 -0800

    adding makeuppercase function to execute when firestore /messages/:documentId/original is added to

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const {setGlobalOptions} = require('firebase-functions');
 const logger = require('firebase-functions/logger');
 const {onRequest} = require('firebase-functions/https');
-const {onDocumentWritten} = require('firebase-functions/v2/firestore');
+const {onDocumentWritten, onDocumentCreated} = require('firebase-functions/v2/firestore');
 
 // firebase Admin SDK to access firestore
 const {initializeApp} = require('firebase-admin/app');
@@ -17,10 +17,20 @@ initializeApp();
  * under the path /messages/:documentId/original
  */
 exports.addmessage = onRequest(async (req, res) => {
-  // grab text param
   const original = req.query.text;
-  // push new message to firestore
-  const writeResult = await getFirestore().collection('messages').add({original: original});
-  // send back success message
+  const writeResult = await getFirestore()
+    .collection('messages')
+    .add({original: original});
   res.json({result:`Message with ID: ${writeResult.id} added.`});
-})
+});
+
+/**
+ * Listends for new messages added to /messages/:documentId/original and saves
+ * an uppercased version of the message to /messages/:documentId/uppercase
+ */
+exports.makeuppercase = onDocumentCreated('/messages/{documentId}', (event) => {
+  const original = event.data.data().original;
+  logger.log('Uppercasing', event.params.documentId, original);
+  const uppercase = original.toUpperCase();
+  return event.data.ref.set({uppercase}, {merge: true});
+});
