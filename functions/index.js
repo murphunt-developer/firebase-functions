4a9600d15da0a91bf526705987ecd65ef5748773
Hunter Murphy <murphunt.developer@gmail.com>
2025-12-01 20:08:40 -0800

    adding addmessage function

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,26 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const {setGlobalOptions} = require('firebase-functions');
+const logger = require('firebase-functions/logger');
+const {onRequest} = require('firebase-functions/https');
+const {onDocumentWritten} = require('firebase-functions/v2/firestore');
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+// firebase Admin SDK to access firestore
+const {initializeApp} = require('firebase-admin/app');
+const {getFirestore} = require('firebase-admin/firestore');
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
+// per-function limit
 setGlobalOptions({ maxInstances: 10 });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/**
+ * Take text parameter passed to this http endpoint and insert it into firestore
+ * under the path /messages/:documentId/original
+ */
+exports.addmessage = onRequest(async (req, res) => {
+  // grab text param
+  const original = req.query.text;
+  // push new message to firestore
+  const writeResult = await getFirestore().collection('messages').add({original: original});
+  // send back success message
+  res.json({result:`Message with ID: ${writeResult.id} added.`});
+})
